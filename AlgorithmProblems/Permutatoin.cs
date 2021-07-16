05c80824585baea9460414d1aee95d0cd50c4fa9
VivekG11 <vivekvk2903@gmail.com>
2021-07-16 12:36:29 +0530

    [Finding permutations of a string]

---
+++ AlgorithmProblems/Permutatoin.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgorithmProblems
+{
+    class Permutatoin
+    {
+        public static void Permute(string str , int start , int last )
+        {
+            if(start == last)
+            {
+                Console.WriteLine(str);
+            }
+            else
+            {
+                for(int i = start; i <= last; i++)
+                {
+                    str = Swap(str, start, i);
+                    Permute(str, start+1, last);
+                    str = Swap(str, start, i);
+                }
+            }
+        }
+
+        public static string Swap(string S , int a , int b)
+        {
+            char temp;
+            char[] charArray = S.ToCharArray();
+            temp = charArray[a];
+            charArray[a] = charArray[b];
+            charArray[b] = temp;
+            string perm = new string(charArray);
+            return perm;
+            
+           
+        }
+    }
+}
---
+++ AlgorithmProblems/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace AlgorithmProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Algorithm Based Problems.....");
+            Console.WriteLine("Enter a string to find possible permutations. ");
+            String str = Console.ReadLine();
+            int len = str.Length;
+            Permutatoin.Permute(str, 0, len-1);
+
+        }
+    }
+}
