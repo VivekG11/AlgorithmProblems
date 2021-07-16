1a239a16faf4843eee373e820f6312ed0609de0f
VivekG11 <vivekvk2903@gmail.com>
2021-07-16 14:00:20 +0530

    [Finding a word in the gievn list]

---
+++ AlgorithmProblems/Binarysearch.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmProblems
+{
+    public class Binarysearch
+    {
+        public static void BinarySearch(List<string> wordList)
+        {
+            int start = 0;
+            int mid = 0;
+            int end = wordList.Count - 1;
+            bool found = false;
+            Console.WriteLine("Enter a word you want to search in list :");
+            string search=Console.ReadLine();
+            while(start<=end)
+            {
+                mid = (start + end) / 2;
+                int res = search.CompareTo(wordList[mid]);
+                if(res == 0)
+                {
+                    Console.WriteLine("Entered word is present.");
+                    found = true;
+                    break;
+                }
+                else if(res>0)
+                {
+                    start = mid + 1;
+                }
+                else
+                {
+                    end = mid - 1;
+                }
+            }
+            if(!found)
+            {
+                Console.WriteLine("Entered word does not exist in the wordlist.");
+            }
+        }
+    }
+}
--- AlgorithmProblems/Program.cs
+++ AlgorithmProblems/Program.cs
@@ -1,4 +1,9 @@
 ﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace AlgorithmProblems
 {
@@ -7,11 +12,15 @@ namespace AlgorithmProblems
         static void Main(string[] args)
         {
             Console.WriteLine("Algorithm Based Problems.....");
-            Console.WriteLine("Enter a string to find possible permutations. ");
-            String str = Console.ReadLine();
-            int len = str.Length;
-            Permutatoin.Permute(str, 0, len-1);
+            //Console.WriteLine("Enter a string to find possible permutations. ");
+            //String str = Console.ReadLine();
+            //int len = str.Length;
+           // Permutatoin.Permute(str, 0, len-1);
 
+            string filePath = File.ReadAllText(@"C:\Users\VIVEK\source\repos\AlgorithmProblems\wordList.txt");
+            List<string> words = new List<string>(filePath.Split(" "));
+            words.Sort();
+            Binarysearch.BinarySearch(words);
         }
     }
 }
